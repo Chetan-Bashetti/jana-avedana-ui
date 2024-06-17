be9cb4687d0bc45828e7afa643d89a0776bb9871
chetankb <chetankb619@gmail.com>
2024-06-17 13:18:53 +0800

    Added new styles to query

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
-const MuiButton = ({ title = '', onClick, type, disabled }) => {
+const MuiButton = ({ title = '', onClick, type, disabled, style }) => {
 	return (
 		<Button
 			variant='contained'
 			style={{
 				fontFamily: "'Montserrat', sans-serif",
-				background: `var(--primary-bg)`,
+				background: '#21b573',
 				fontSize: '0.8em',
 				fontWeight: 600,
 				padding: '1em 4em',
 				textTransform: 'capitalize',
 				borderRadius: '0.7em',
-				boxShadow: '1px 10px 20px #1f5ae37d',
-				border: '1px solid white'
+				boxShadow: '0px',
+				...style
 			}}
 			onClick={onClick}
 			type={type}
